091bf9f3d8ec67e56c16346aa3e2fdb9fa2e1eb3
Savindi Wijenayaka <savindi.narmada@gmail.com>
2020-12-09 14:55:05 +0530

    add model logging

--- run_clm_modified.py
+++ run_clm_modified.py
@@ -1,17 +1,3 @@
-# coding=utf-8
-# Copyright 2020 The HuggingFace Inc. team. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 """
 Fine-tuning the library models for causal language modeling (GPT, GPT-2, CTRL, ...) on a text file or a dataset.
 
@@ -170,6 +156,8 @@ def main():
 #     if is_main_process(training_args.local_rank):
 #         transformers.utils.logging.set_verbosity_info()
     logger.info("Training/evaluation parameters %s", training_args)
+    logger.info("Model parameters %s", model_args)
+    logger.info("Data parameters %s", data_args)
 
     # Set seed before initializing model.
     set_seed(training_args.seed)
